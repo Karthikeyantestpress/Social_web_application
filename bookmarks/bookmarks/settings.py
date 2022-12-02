e57cc3118894d0682bf6e0533e64548bb0931f29
Karthik <karthikeyan@testpress.in>
2022-12-02 16:04:18 +0530

    Add custom login view

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.AllowAllUsersModelBackend"
+]
 
 ROOT_URLCONF = "bookmarks.urls"
 
