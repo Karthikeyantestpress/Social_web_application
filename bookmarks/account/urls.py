598bdbdb3b653cd3a76b89d99375423dabf8f244
Karthik <karthikeyan@testpress.in>
2022-12-14 11:39:49 +0530

    Create detail view for users

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("edit/", views.edit, name="edit"),
     path("users/", views.user_list, name="user_list"),
+    path("users/<username>/", views.user_detail, name="user_detail"),
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
 ]
